a5dd33372df99d4d59bc022d40d791d2edbbaa21
bansitrt <bansitrt@gamil.com>
2022-05-18 23:27:57 +0530

    graphql

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,56 @@
+/* eslint-disable prettier/prettier */
+
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+
+import { StudentService } from '../student/student.service';
+import { assignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
+
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentToLessonInput: assignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentToLessonInput;
+    return this.lessonService.assignStudentToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
+  }
+}
